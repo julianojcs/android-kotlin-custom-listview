279bf971abcf19d6277d22bffd5541019885ef66
Juliano Costa Silva <apfjuliano@gmail.com>
2022-09-19 22:12:31 -0300

    refactor: viewHolder implementation

--- app/src/main/java/com/apfjuliano/userlist/ListAdapter.kt
+++ app/src/main/java/com/apfjuliano/userlist/ListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class ListAdapter(private val users: MutableList<User>): BaseAdapter() {
     override fun getCount(): Int {
@@ -20,17 +21,34 @@ class ListAdapter(private val users: MutableList<User>): BaseAdapter() {
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val layoutInflater = LayoutInflater.from(parent?.context)
-        val view = layoutInflater.inflate(R.layout.user_item, parent, false)
+        val view: View
+        val viewHolder: ViewHolder
+        if (convertView == null) { // first time
+            val layoutInflater = LayoutInflater.from(parent?.context)
+            view = layoutInflater.inflate(R.layout.user_item, parent, false)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
 
-        val tvName = view.findViewById<TextView>(R.id.tvName)
-        val tvAge = view.findViewById<TextView>(R.id.tvAge)
-        val tvId = view.findViewById<TextView>(R.id.tvId)
-
-        tvName.text = users[position].name
-        tvAge.text = users[position].age.toString()
-        tvId.text = users[position].id.toString()
+        viewHolder.tvName.text = users[position].name
+        viewHolder.tvAge.text = users[position].age.toString()
+        viewHolder.tvId.text = users[position].id.toString()
 
         return view
+    }
+}
+
+class ViewHolder(view: View) {
+    val tvName: TextView
+    val tvAge: TextView
+    val tvId: TextView
+
+    init {
+        tvName = view.findViewById(R.id.tvName)
+        tvAge = view.findViewById(R.id.tvAge)
+        tvId = view.findViewById(R.id.tvId)
     }
 }
