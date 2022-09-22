e2fc242099523b60229f2d024be206398fdbc511
Juliano Costa Silva <apfjuliano@gmail.com>
2022-09-22 18:18:08 -0300

    feat: Shared Preferences storage

--- app/src/main/java/com/apfjuliano/userlist/SampleActivity.kt
+++ app/src/main/java/com/apfjuliano/userlist/SampleActivity.kt
@@ -1,11 +1,16 @@
 package com.apfjuliano.userlist
 
+import android.content.Context
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+
+private const val PREF_EMAIL = "email"
+private const val PREF_SMS = "sms"
+private const val PREF_AGE = "age"
+private const val PREF = "STORAGE_PREFERENCES" //Name of stored preferences
 
 class SampleActivity: AppCompatActivity() {
 
@@ -15,6 +20,11 @@ class SampleActivity: AppCompatActivity() {
 
         val ivProfile = findViewById<ImageView>(R.id.iv_profile)
         val tvName = findViewById<TextView>(R.id.tv_name)
+        val chkEmail = findViewById<CheckBox>(R.id.chkEmail)
+        val chkSMS = findViewById<CheckBox>(R.id.chkSms)
+        val edtAge = findViewById<EditText>(R.id.edtAge)
+        val btnSave = findViewById<Button>(R.id.btnSave)
+
         val url = "https://github.com/julianojcs.png"
 
         Glide.with(this)
@@ -23,5 +33,19 @@ class SampleActivity: AppCompatActivity() {
             .into(ivProfile)
 
         tvName.text = getString(R.string.profile_name)
+
+        val preferences = getSharedPreferences(PREF, Context.MODE_PRIVATE)
+
+        chkEmail.isChecked = preferences.getBoolean(PREF_EMAIL, false)
+        chkSMS.isChecked = preferences.getBoolean(PREF_SMS, false)
+        edtAge.setText(preferences.getInt(PREF_AGE, 0).toString())
+
+        btnSave.setOnClickListener {
+            val edit =preferences.edit()
+            edit.putBoolean(PREF_EMAIL, chkEmail.isChecked)
+            edit.putBoolean(PREF_SMS, chkSMS.isChecked)
+            edit.putInt(PREF_AGE, edtAge.text.toString().toInt())
+            edit.apply()
+        }
     }
 }
